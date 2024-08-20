75fa0187289d79a8c5b2aa3f6cdb063026ee168e
lancerharris <lancerprogram@gmail.com>
2024-08-20 11:36:28 -0700

    Use groups to manage objects

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
